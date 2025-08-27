94894bfe2a7ff034aee11093bc9e100b7dd1cf1d
e88gt <benshariz.18@gmail.com>
2025-08-27 10:40:02 +0800

    First file

---
+++ src/my/e88gt/schlong/Schlong.java
@@ -0,0 +1,24 @@
+package my.e88gt.schlong;
+
+import java.nio.file.*;
+
+public class Schlong
+{
+	public static void main(String...args)throws Throwable
+	{
+		args=new String[] {""};
+		String path=args[0],src=new String(Files.readAllBytes(Paths.get(path)));
+		
+		looping:for(int i=0;i<src.length();i++)
+		{
+			// who Is going To be Modifying this?
+			// and When is He going To modify It?
+			final char c=src.charAt(i);
+			
+			if(Character.isWhitespace(c))
+			{
+				continue looping;
+			}
+		}
+	}
+}
